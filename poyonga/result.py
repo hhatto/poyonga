d3aef9597ad610d7ed79836b5dd56049004c2f3f
Hideo Hattori <hhatto.jp@gmail.com>
2012-02-05 00:04:48 +0900

    fixed: parse result. featured: implement GroongaSelectResult object

--- poyonga/result.py
+++ poyonga/result.py
@@ -10,7 +10,18 @@ class GroongaResult(object):
         self.status = _result[0][0]
         self.start_time = _result[0][1]
         self.elapsed = _result[0][2]
-        if len(_result) == 1:
+        if len(_result) == 1 and self.status != 0:
             self.body = _result[0][3]
+        elif len(_result) == 1 and self.status == 0:
+            self.body = ""  # handling invalid result
         else:
             self.body = _result[1]
+
+
+class GroongaSelectResult(GroongaResult):
+
+    def __init__(self, data, output_type="json"):
+        super(GroongaSelectResult, self).__init__(data, output_type)
+        self.hit_num = self.body[0][0][0]
+        keys = [k[0] for k in self.body[0][1]]
+        self.items = [dict(zip(keys, item)) for item in self.body[0][2:]]
