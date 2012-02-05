437c5a670902aac7800750eb95fd644f5ee679a3
Hideo Hattori <hhatto.jp@gmail.com>
2012-02-05 22:48:45 +0900

    fix: parse select's data when only status:0

--- poyonga/result.py
+++ poyonga/result.py
@@ -22,6 +22,8 @@ class GroongaSelectResult(GroongaResult):
 
     def __init__(self, data, output_type="json"):
         super(GroongaSelectResult, self).__init__(data, output_type)
+        self.items = []
         self.hit_num = self.body[0][0][0]
-        keys = [k[0] for k in self.body[0][1]]
-        self.items = [dict(zip(keys, item)) for item in self.body[0][2:]]
+        if self.status == 0:
+            keys = [k[0] for k in self.body[0][1]]
+            self.items = [dict(zip(keys, item)) for item in self.body[0][2:]]
