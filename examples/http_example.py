269f1b743583609ecdc7658e35073db9b985634c
Hideo Hattori <hhatto.jp@gmail.com>
2012-02-08 03:19:05 +0900

    rename http.py

---
+++ examples/http_example.py
@@ -0,0 +1,13 @@
+from poyonga.client import Groonga
+
+g = Groonga()
+
+cmds = [("status", {}),
+        ("log_level", {"level": "warning"}),
+        #("table_create", {"name": "Site", "flags": "TABLE_HASH_KEY"}),
+        ("select", {"table": "Site"})]
+for cmd, kwargs in cmds:
+    ret = g.call(cmd, **kwargs)
+    print(ret.status)
+    print(ret.body)
+    print("*" * 40)
