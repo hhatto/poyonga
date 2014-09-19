5c5b528428b58ef4dd07ae2d2acbf24bf04aecad
Hideo Hattori <hattori-h@klab.com>
2014-09-19 12:51:46 +0900

    fix: execption when get error response with select command

--- poyonga/result.py
+++ poyonga/result.py
@@ -54,7 +54,9 @@ class GroongaSelectResult(GroongaResult):
     def __init__(self, data, output_type="json", encoding='utf-8'):
         super(GroongaSelectResult, self).__init__(data, output_type, encoding)
         self.items = []
-        self.hit_num = self.body[0][0][0]
+        self.hit_num = -1   # default is -1 (Error)
+        if len(self.body) != 0:
+            self.hit_num = self.body[0][0][0]
         if self.status == 0:
             keys = [k[0] for k in self.body[0][1]]
             self.items = [
