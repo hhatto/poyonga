d96c6a2fe0960a9f79e2a2849d533545432c19c4
Hideo Hattori <hhatto.jp@gmail.com>
2021-01-17 11:48:59 +0900

    add example of pyarrow version

---
+++ examples/groonga_microblog_tutorial/3_search_with_pyarrow.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+from poyonga import Groonga
+
+
+def _call_with_apachearrow(g, cmd, **kwargs):
+    print("[http with apache arrow response]")
+    ret = g.call(cmd, **kwargs)
+    print("status:", ret.status)
+    if cmd == "select":
+        print("item:", len(ret.items))
+        print(type(ret.items))
+        print("pydict:", ret.items.to_pydict())
+        for item in ret.items:
+            print(item)
+        print("=*=" * 30)
+    else:
+        print(ret.body)
+
+
+def _call(g, cmd, **kwargs):
+    print("[http with json response]")
+    ret = g.call(cmd, **kwargs)
+    print("status:", ret.status)
+    if cmd == "select":
+        print("item:", len(ret.items))
+        print(type(ret.items))
+        for item in ret.items:
+            print(item)
+        print("=*=" * 30)
+    else:
+        print(ret.body)
+
+
+g = Groonga()
+
+# _call(
+#     g,
+#     "select",
+#     table="Users",
+#     match_columns="name,location_str,description",
+#     query="東京",
+#     output_columns="_key,name",
+# )
+# _call_with_apachearrow(
+#     g,
+#     "select",
+#     table="Users",
+#     match_columns="name,location_str,description",
+#     query="東京",
+#     output_type="apache-arrow",
+#     output_columns="_key,name",
+# )
+_call(
+    g,
+    "select",
+    table="Comments",
+    filter="last_modified<=1268802000",
+    output_columns="posted_by.name,comment,last_modified",
+    drilldown="hash_tags,posted_by",
+)
+_call_with_apachearrow(
+    g,
+    "select",
+    table="Comments",
+    filter="last_modified<=1268802000",
+    output_columns="posted_by.name,comment,last_modified",
+    output_type="apache-arrow",
+    drilldown="hash_tags,posted_by",
+    drilldown_output_column="_id",
+)
