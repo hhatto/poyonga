4232ed53b52200aade02aeb4d4a3217a83400c84
Hideo Hattori <hhatto.jp@gmail.com>
2023-11-28 20:04:23 +0900

    version 0.6

--- poyonga/__init__.py
+++ poyonga/__init__.py
@@ -2,6 +2,6 @@ from poyonga.client import Groonga
 from poyonga.const import InputType, OutputType
 from poyonga.result import GroongaResult
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 __all__ = ["Groonga", "GroongaResult", "InputType", "OutputType"]
